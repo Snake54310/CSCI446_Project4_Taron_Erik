56ff7ed2d8401b93878739e4d612d8dfaac35448
Snake54310 <86499062+Snake54310@users.noreply.github.com>
2025-12-03 21:05:54 -0700

    made output create text file. Code should be done.

--- CSCI446_Project4_main_Group27.py
+++ CSCI446_Project4_main_Group27.py
@@ -6,6 +6,7 @@ import sys
 import re
 import copy as cp
 from Track import Track
+import matplotlib.pyplot as plt
 
 def fileImport(fileName): # brings file into program as str numpy array
     inputText = ''
@@ -57,30 +58,74 @@ def createTrack(inputTextArray, CRASH_POS):
     
     return track
 
-def saveOutput(GROUP_ID, ALGORITHM, TRACK_NAME, CRASH_POS, track): 
-    # saves solved network reported variables to output file
-    # Formatted name as: [GROUP_ID]_[ALGORITHM]_[TRACK_NAME]_[CRASH_POS].txt
+import matplotlib.pyplot as plt   # ← only this import is needed
+
+def saveOutput(GROUP_ID, ALGORITHM, TRACK_NAME, CRASH_POS, track):
+
     trackPathArr = TRACK_NAME.split('/')
     trackName = trackPathArr[-1]
-    fileName = GROUP_ID + "_" + ALGORITHM + "_" + trackName[:-4] + "_" + CRASH_POS + ".txt"
-    writeArray = track.getInputTextArray()
+    # txtFileName = GROUP_ID + "*" + ALGORITHM + "*" + trackName[:-4] + "*" + CRASH_POS + ".txt"
+    
+    writeArray = track.getInputTextArray().copy() 
     path = track.getBestPath()
     writeString = ''
-
+    
     for pos in path:
         writeArray[pos[0]][pos[1]] = 'P'
-
+    
     for row in writeArray:
         for col in row:
             writeString += col
         writeString += '\n'
     
-    with open(fileName, "w") as f:
-        f.write(writeString)
-        
+    #with open(txtFileName, "w") as f:
+        #f.write(writeString)
+    
     print("Best Path Taken: ")
     print(writeString)
+
+    pngFileName = GROUP_ID + "_" + ALGORITHM + "_" + trackName[:-4] + "_" + CRASH_POS + ".png"
     
+    grid = track.getInputTextArray()
+    rows, cols = grid.shape
+
+    fig = plt.figure(figsize=(max(8, cols/10), max(8, rows/10)), dpi=200)
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.set_xlim(0, cols)
+    ax.set_ylim(0, rows)
+    ax.set_aspect('equal')
+    ax.invert_yaxis()
+    ax.axis('off')
+
+    # Draw tiles
+    for r in range(rows):
+        for c in range(cols):
+            char = grid[r, c]
+            color = {'#': 'gray', 'F': 'limegreen', 'S': 'royalblue'}.get(char, 'white')
+            ax.add_patch(plt.Rectangle((c, r), 1, 1,
+                                       facecolor=color, edgecolor='black', linewidth=0.5))
+
+            # Small yellow square only on original '.' cells that are on the path
+            if (r, c) in path and grid[r, c] == '.':
+                ax.add_patch(plt.Rectangle((c + 0.25, r + 0.25), 0.5, 0.5,
+                                           facecolor='yellow', edgecolor='orange', linewidth=1))
+
+    # Gold line connecting path centers
+    if len(path) > 1:
+        x = [p[1] + 0.5 for p in path]
+        y = [p[0] + 0.5 for p in path]
+        ax.plot(x, y, color='gold', linewidth=6, solid_capstyle='round')
+
+    # Dark circles on Start and Finish
+    if path:
+        sr, sc = path[0]
+        fr, fc = path[-1]
+        ax.add_patch(plt.Circle((sc + 0.5, sr + 0.5), 0.4, color='darkblue'))
+        ax.add_patch(plt.Circle((fc + 0.5, fr + 0.5), 0.4, color='darkgreen'))
+
+    ax.set_title(f"{ALGORITHM} – {track.getBestMoves()} moves", fontsize=14, pad=20)
+    plt.savefig(pngFileName, dpi=300, bbox_inches='tight')
+    plt.close(fig)
     return
 
 def main(GROUP_ID, ALGORITHM, TRACK_NAME, CRASH_POS): 
